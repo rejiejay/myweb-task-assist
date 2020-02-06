f22811ee272e73a319831ea4be2d62973ee58b7b
rejiejay <454766952@qq.com>
2020-02-06 15:45:11 +0800

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    init.go()
+}
+
+
+/**
+ * 初始化方法
+ */
+var init = {
+    go: function go() {
+        this.initDom()
+    },
+
+    /**
+     * 节点 初始化
+     */
+    initDom: function initDom() {}
+}
+
+/**
+ * 按钮
+ */
+var button = {
+    dom: null,
+
+    init: function init() {
+        var self = this
+    },
+}
