c6f4c25a184c9ad052f4b45f16d8565dda24a953
rejiejay <454766952@qq.com>
2020-02-07 22:28:49 +0800

    add href edit

--- views/todo/index.js
+++ views/todo/index.js
@@ -8,10 +8,35 @@ window.onload = function () {
 var init = {
     go: function go() {
         this.initDom()
+
+        components.init()
+        edit.init()
     },
 
     /**
      * 节点 初始化
      */
-    initDom: function initDom() {}
+    initDom: function initDom() {
+        edit.dom = document.getElementById('edit-todo')
+    }
+}
+
+var components = {
+    toast: null,
+
+    init: function init() {
+        this.toast = Toast.init()
+    }
+}
+
+var edit = {
+    dom: null,
+
+    init: function init() {
+        this.dom.onclick = function () {
+            components.toast.show()
+            window.location.href = './edit/index.html'
+            components.toast.destroy()
+        }
+    }
 }
