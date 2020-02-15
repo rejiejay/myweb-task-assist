72fec7f975d7989288263bf2cfd2394aa4331eee
rejiejay <454766952@qq.com>
2020-02-15 08:39:34 +0800

    add Plan Eidt

--- views/plan/index.js
+++ views/plan/index.js
@@ -9,14 +9,28 @@ var initialization = {
     main: function main() {
         components.init()
         this.initDom()
+
+        edit.init()
     },
 
     /**
      * 节点 初始化
      */
-    initDom: function initDom() {},
+    initDom: function initDom() {
+        edit.dom = document.getElementById('edit-add')
+    },
 }
 
 var components = {
     init: function init() {}
+}
+
+var edit = {
+    dom: null,
+
+    init: function init() {
+        this.dom.onclick = function () {
+            window.location.href = './edit/index.html'
+        }
+    }
 }
