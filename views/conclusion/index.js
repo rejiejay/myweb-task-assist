85cf8bf0a75ddeaad16e6f91baf13cabd135e5a9
rejiejay <454766952@qq.com>
2020-02-26 00:25:46 +0800

    init conclusion

--- views/conclusion/index.js
+++ views/conclusion/index.js
@@ -3,6 +3,16 @@ window.onload = function () {
 }
 
 var CONST = {
+    TARGET: {
+        DEFAULTS: {
+            id: '',
+            name: '所有'
+        },
+        DEMO: {
+            id: 'gwy',
+            name: '公务员'
+        }
+    },
     SORT: {
         TIME: {
             value: 'time',
@@ -22,12 +32,20 @@ var CONST = {
  */
 var initialization = {
     main: function main() {
-        components.init()
+        var self = this
+
         this.initDom()
+        components.init()
 
         sort.init()
         edit.init()
+
+        process.init().then(() => {
+            self.stepTwo()
+        })
     },
+
+    stepTwo: function stepTwo() {},
 
     /**
      * 节点 初始化
@@ -35,15 +53,90 @@ var initialization = {
     initDom: function initDom() {
         sort.dom = document.getElementById('sort')
         edit.dom = document.getElementById('conclusion-add')
-    },
+
+        process.description_dom = document.getElementById('process-description')
+        process.unlock_dom = document.getElementById('process-unlock')
+    }
 }
 
 var components = {
     constHandle: null,
+    fetch: null,
+    serviceStorage: null,
 
     init: function init() {
         this.constHandle = ConstHandle
+        this.fetch = Fetch.init()
+        this.serviceStorage = ServiceStorage.init()
     }
+}
+
+
+var process = {
+    target: CONST.TARGET.DEFAULTS,
+    description_dom: null,
+    unlock_dom: null,
+
+    init: function init() {
+        var self = this
+
+        this.initUnlockHandle()
+
+        return new Promise((resolve) => {
+            self.initProcessTarget(resolve)
+        })
+    },
+
+    initUnlockHandle: function initUnlockHandle() {
+        var self = this
+        this.unlock_dom.onclick = function () {
+            if (!self.target.id) return
+
+            var handle = function handle() {
+                self.unlock()
+            }
+
+            var parameter = {
+                title: '你确认要解除锁定吗?',
+                succeedHandle: handle
+            }
+            components.confirmPopUp(parameter)
+        }
+    },
+
+    initProcessTarget: function initProcessTarget(resolve) {
+        var self = this
+
+        components.serviceStorage.getItem({
+            key: 'processTarget',
+            hiddenError: true
+        }).then(
+            res => {
+                self.target = res
+                self.render()
+                resolve()
+            },
+            error => resolve()
+        )
+    },
+
+    unlock: function unlock() {
+        var self = this
+
+        components.serviceStorage.clearItem({
+            key: 'processTarget'
+        }).then(
+            res => {
+                self.target = CONST.TARGET.DEFAULTS
+                self.render()
+            },
+            error => {}
+        )
+    },
+
+    render: function render() {
+        this.description_dom.innerHTML = `范围: ${this.target.name}`
+    },
 }
 
 var sort = {
