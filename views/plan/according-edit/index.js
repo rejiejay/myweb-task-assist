e716187d646d30eb8f59053675ce32e80dd0d88e
rejiejay <454766952@qq.com>
2020-02-25 23:25:22 +0800

    add 3 api debug

--- views/plan/according-edit/index.js
+++ views/plan/according-edit/index.js
@@ -12,6 +12,24 @@ var CONST = {
             id: 'gwy',
             name: '公务员'
         }
+    },
+    PAGE_STATUS: {
+        DEFAULTS: 'add',
+        ADD: 'add',
+        EDIT: 'edit'
+    },
+    PLAN: {
+        DEFAULTS: {
+            id: 0,
+            program: null
+        },
+        DEMO: {
+            id: 0,
+            targetId: 'not-null',
+            program: null,
+            according: null,
+            sqlTimestamp: 'not-null'
+        }
     }
 }
 
@@ -19,6 +37,7 @@ var CONST = {
  * 初始化方法
  */
 var initialization = {
+    status: CONST.PAGE_STATUS.DEFAULTS,
     clientWidth: 375,
     clientHeight: 667,
 
@@ -31,30 +50,51 @@ var initialization = {
         components.init()
         this.initDom()
 
-        textarea.init()
+        this.initPageStatus()
+
+        confirm.init()
+        del.init()
 
         process.init().then(() => {
             self.stepTwo()
         }, error => {})
     },
 
-    stepTwo: function stepTwo() {},
+    stepTwo: function stepTwo() {
+        textarea.init()
+    },
+
+    initPageStatus: function initPageStatus() {
+        var id = window.localStorage['task-plan-according-id']
+
+        if (id) {
+            textarea.data.id = id
+            this.status = CONST.PAGE_STATUS.EDIT
+            confirm.dom.innerHTML = '编辑'
+        }
+
+        window.localStorage['task-plan-according-id'] = ''
+    },
 
     /**
      * 节点 初始化
      */
     initDom: function initDom() {
         textarea.dom = document.getElementById('according-input')
+        confirm.dom = document.getElementById('confirm')
+        del.dom = document.getElementById('delete')
     },
 }
 
 var components = {
     fetch: null,
     serviceStorage: null,
+    confirmPopUp: null,
 
     init: function init() {
         this.fetch = Fetch.init()
         this.serviceStorage = ServiceStorage.init()
+        this.confirmPopUp = ConfirmPopUp.init()
     }
 }
 
@@ -87,8 +127,121 @@ var process = {
 
 var textarea = {
     dom: null,
+    data: CONST.PLAN.DEFAULTS,
 
     init: function init() {
+        var self = this
         textarea.dom.style.height = `${initialization.clientHeight - 46}px`
+
+        this.dom.oninput = function () {
+            self.data.according = this.value
+        }
+
+        if (initialization.status === CONST.PAGE_STATUS.EDIT) this.initDate();
+    },
+
+
+    initDate: function initDate() {
+        var self = this
+        components.fetch.get({
+            url: 'plan/get/one',
+            query: {
+                id: this.data.id
+            }
+        }).then(
+            res => {
+                self.data = res.data
+                self.data.according = res.data.according
+                self.dom.value = res.data.according
+            },
+            error => {}
+        )
+    },
+
+}
+
+var confirm = {
+    dom: null,
+
+    init: function init() {
+        var self = this
+
+        this.dom.onclick = function () {
+            self.verify()
+        }
+    },
+
+    verify: function verify() {
+        var content = textarea.data.according
+        var status = initialization.status
+
+        if (!content) return components.toast.show('内容不能为空');
+
+        if (status === CONST.PAGE_STATUS.EDIT) {
+            return this.editHandle()
+        }
+
+        if (status === CONST.PAGE_STATUS.ADD) {
+            return this.addHandle()
+        }
+    },
+
+    editHandle: function editHandle() {
+        components.fetch.post({
+            url: 'plan/according/edit',
+            body: {
+                id: textarea.data.id,
+                according: textarea.data.according
+            }
+        }).then(
+            res => window.location.href = './../according-list/index.html',
+            error => {}
+        )
+    },
+
+    addHandle: function addHandle() {
+        components.fetch.post({
+            url: 'plan/add',
+            body: {
+                targetId: process.target.id,
+                according: textarea.data.according
+            }
+        }).then(
+            res => window.location.href = './../according-list/index.html',
+            error => {}
+        )
+    }
+}
+
+var del = {
+    dom: null,
+
+    init: function init() {
+        var self = this
+        var isDeling = false
+
+        if (status === CONST.PAGE_STATUS.ADD) return this.dom.style = 'display: none;';
+        this.dom.onclick = function () {
+            if (isDeling) return
+            isDeling = true
+
+            var parameter = {
+                title: `确认要删除?`,
+                succeedHandle: () => self.delHandle
+            }
+            components.confirmPopUp(parameter)
+        }
+    },
+
+    delHandle: function delHandle() {
+        components.fetch.post({
+            url: 'plan/del',
+            body: {
+                id: textarea.data.id,
+            }
+        }).then(
+            res => window.location.href = './../according-list/index.html',
+            error => {}
+        )
     }
 }
