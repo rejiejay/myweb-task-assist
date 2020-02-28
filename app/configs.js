6c28a6570ba8672254d40a4c26a2786f6c651132
rejiejay <454766952@qq.com>
2020-02-28 18:27:20 +0800

    add gulp less render

--- app/configs.js
+++ app/configs.js
@@ -3,15 +3,6 @@
  */
 export const configs = [{
     route: '/index.html',
-    webpack: {
-        entry: './views/index.jsx',
-        output: {
-            path: './build',
-            filename: 'index.js'
-        }
-    },
-    html: {
-        entry: './views/index.art',
-        output: './build/index.html'
-    },
+    entry: './views',
+    output: './build'
 }]
--- app/render.js
+++ app/render.js
@@ -6,43 +6,57 @@ import fs from 'fs';
 import path from 'path';
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 import template from 'art-template';
+import gulp from 'gulp';
+import less from 'gulp-less';
+import LessPluginCleanCSS from 'less-plugin-clean-css';
 
 const __dirname = path.resolve(path.dirname(''));
 const buildPath = myPath => path.join(__dirname, path.relative(__dirname, myPath));
 
-const html = async config => {
-    return await new Promise((resolve, reject) => fs.readFile(buildPath(config.html.entry), 'utf8', (readFileError, content) => {
-        if (readFileError) return reject(readFileError);
+const cleanCSSPlugin = new LessPluginCleanCSS({
+    advanced: true
+});
 
-        /** https://aui.github.io/art-template/zh-cn/docs/api.html */
-        const html = template.render(content);
+const html = async config => await new Promise((resolve, reject) => fs.readFile(buildPath(`${config.entry}/index.art`), 'utf8', (readFileError, content) => {
+    if (readFileError) return reject(readFileError);
 
-        fs.writeFile(buildPath(config.html.output), html, {
-            encoding: 'utf8'
-        }, writeFileError => {
-            if (writeFileError) return reject(writeFileError);
-            resolve()
-        });
-    })).then(
-        () => consequencer.success(),
-        error => consequencer.error(error)
-    )
-}
+    /** https://aui.github.io/art-template/zh-cn/docs/api.html */
+    const html = template.render(content);
 
-const less = async config => {
+    fs.writeFile(buildPath(`${config.output}/index.html`), html, {
+        encoding: 'utf8'
+    }, writeFileError => {
+        if (writeFileError) return reject(writeFileError);
+        resolve()
+    });
+})).then(
+    () => consequencer.success(),
+    error => consequencer.error(error)
+)
 
-}
+const css = async config => await new Promise((resolve, reject) => gulp.src(buildPath(`${config.entry}/index.less`))
+    .pipe(less({
+        paths: buildPath('./css'),
+        plugins: [cleanCSSPlugin] /** 压缩 */
+    }))
+    .pipe(gulp.dest(config.output))
+    .on('error', error => reject(`转换 less 文件出错, 原因: ${error}`))
+    .on('end', () => resolve())
+).then(
+    () => consequencer.success(),
+    error => consequencer.error(error)
+)
 
 const javaScript = async config => {
     return await new Promise((resolve, reject) => {
         webpack({
             entry: [
-                path.join(__dirname, path.relative(__dirname, config.webpack.entry))
+                buildPath(`${config.entry}/index.jsx`)
             ],
             output: {
                 publicPath: './',
-                path: path.join(__dirname, path.relative(__dirname, config.webpack.output.path)),
-                filename: config.webpack.output.filename
+                path: buildPath(config.output),
+                filename: 'index.js'
             },
             module: {
                 rules: [{
@@ -73,6 +87,9 @@ export const render = async config => {
 
     const htmlInstance = await html(config);
     if (htmlInstance.result !== 1) return htmlInstance;
+
+    const cssInstance = await css(config);
+    if (cssInstance.result !== 1) return cssInstance;
 
     return consequencer.success()
 }
