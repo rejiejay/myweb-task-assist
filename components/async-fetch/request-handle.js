7580c7c09ede15c2a08471c5761eba29a2127519
rejiejay <454766952@qq.com>
2020-03-01 11:00:20 +0800

    add asyncRequestHandle

---
+++ components/async-fetch/request-handle.js
@@ -0,0 +1,20 @@
+import toast from '../toast.js';
+import {
+    inputPopUp,
+    inputPopUpDestroy
+} from '../input-popup.js';
+
+import config from './config.js';
+import initHeaders from './headers.js';
+import queryToUrl from './url-handle.js';
+
+
+const requestHandle = ({
+    method,
+    parameter,
+    resolve,
+    reject
+}) => {}
+
+
+export default requestHandle
