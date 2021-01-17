0416aa0349f710a73b2b660654189a91e0ea7536
rejiejay <454766952@qq.com>
2021-01-17 15:20:30 +0800

    add top bottom operate

--- view/page/index.jsx
+++ view/page/index.jsx
@@ -1,13 +1,14 @@
 import deviceHandle from './../utils/device-handle'
 
 window.onload = () => {
-    if (deviceHandle.isMobile()) return import('./mobile.jsx').then(module => {
-        const MobileComponent = module.MobileComponent
-        ReactDOM.render(<MobileComponent />, document.getElementById('jeker-task-assist-system'))
+    const root = document.getElementById('jeker-task-assist-system')
+    if (deviceHandle.isMobile()) return import('./mobile.jsx').then(({ MobileComponent }) => {
+        root.className = 'mobile-device-container'
+        ReactDOM.render(<MobileComponent />, root)
     })
 
-    import('./web.jsx').then(module => {
-        const WebComponent = module.WebComponent
-        ReactDOM.render(<WebComponent />, document.getElementById('jeker-task-assist-system'))
+    import('./web.jsx').then(({ WebComponent }) => {
+        root.className = 'web-device-container'
+        ReactDOM.render(<WebComponent />, root)
     })
 }
--- view/page/mobile.jsx
+++ view/page/mobile.jsx
@@ -2,10 +2,35 @@ export class MobileComponent extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            isShowBigCar: true
+        }
     }
 
     render() {
-        return <div>MobileComponent</div>
+        const { isShowBigCar } = this.state
+
+        return <>
+            <div className='list-top-operate flex-start-center'>
+                <div className='top-operate-navigation flex-start flex-rest'>
+                    <div className='list-top-button'>Navigation</div>
+                </div>
+                <div className='switch-car-size'>
+                    <div className='list-top-button'>{isShowBigCar ? 'Switch Small Car' : 'Switch Big Car'}</div>
+                </div>
+            </div>
+
+            <div className='list-content'>
+            </div>
+
+            <div className='list-bottom-operate flex-start-center'>
+                <div className='bottom-operate-filter flex-start flex-rest'>
+                    <div className='list-bottom-button right-line'>Filter</div>
+                </div>
+                <div className='bottom-operate-sort'>
+                    <div className='list-bottom-button left-line'>Sort</div>
+                </div>
+            </div>
+        </>
     }
 }
