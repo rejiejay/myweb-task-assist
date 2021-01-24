0ea04e5aeea5864d06b4ca6cf21407e8ac6b05d9
rejiejay <454766952@qq.com>
2021-01-25 01:01:00 +0800

    update long-term-task

--- view/page/entry/common-components/filter-edit.js
+++ view/page/entry/common-components/filter-edit.js
@@ -1,5 +1,6 @@
 import CommonlyListItem from './../../../components/mobile/commonly-list-item'
 import Button from './../../../components/button'
+import ActionSheet from './../../../components/action-sheet'
 import jsxStyle from './../../../components/jsx-style'
 
 import service from './../service'
@@ -58,13 +59,37 @@ class FilterEdit extends React.Component {
         this.setState({ longTermOptions, longTermFilter })
     }
 
+    selectLongTermTaskHandle = async () => {
+        const { longTermOptions } = this.state
+        const selectInstance = await ActionSheet({ title: '请选择 Long Term Task', options: longTermOptions })
+        if (selectInstance.result !== 1) return
+        const longTerm = selectInstance.data
+        this.setState({ longTermFilter: { id: longTerm.value, des: longTerm.label } })
+    }
+
+    cancelSelectLongTermTaskHandle = () => this.setState({ longTermFilter: { id: null, des: '' } })
+
     render() {
+        const { longTermFilter } = this.state
+
         return <div className='filter-edit-container'>
             <CommonlyListItem key='long-term-task'
                 title='是否为(Long Term Task)项目?'
             >
-                <Button style={{ ...jsxStyle.basicFlex.startCenter }}
-                >Long Term Task</Button>
+                <>
+                    <Button style={{ ...jsxStyle.basicFlex.startCenter }}
+                        onClick={this.selectLongTermTaskHandle}
+                    >
+                        {longTermFilter.id ?
+                            `属于长期项目: ${longTermFilter.des}` :
+                            '不属于长期任务项目'
+                        }
+                    </Button>
+                    <div style={{ height: '5px' }}></div>
+                    {longTermFilter.id && <Button
+                        onClick={this.cancelSelectLongTermTaskHandle}
+                    >取消选择长期</Button>}
+                </>
             </CommonlyListItem>
             <CommonlyListItem key='time-filter'
                 title='时间过滤器'
@@ -81,6 +106,16 @@ class FilterEdit extends React.Component {
             >
                 <div>Task Filter</div>
             </CommonlyListItem>
+            <div style={{ height: '15px' }}></div>
+            <div style={{ borderTop: '1px solid #ddd' }}></div>
+            <div style={{ height: '15px' }}></div>
+            <Button
+                style={{ backgroundColor: '#1890ff' }}
+            >确认</Button>
+            <div style={{ height: '5px' }}></div>
+            <Button
+                style={{ backgroundColor: '#F2F2F2', color: '#626675' }}
+            >取消</Button>
         </div>
     }
 }
