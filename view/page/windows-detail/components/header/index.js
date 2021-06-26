604a493782122bcf621ece2044f0ac7e65ce2855
rejiejay <454766952@qq.com>
2021-06-26 16:53:56 +0800

    update windows-header

--- view/page/windows-detail/components/header/index.js
+++ view/page/windows-detail/components/header/index.js
@@ -5,6 +5,15 @@ export default class Header extends React.Component {
     }
 
     render() {
-        return <div className='windows-header flex-start-center noselect'>header</div>
+        return <div className='windows-header flex-start-center noselect'>
+            <div className="left-operating flex-start-center flex-rest">
+                <div className="operat-item hover-item">标签分类</div>
+                <div className="operat-item hover-item">优先级</div>
+            </div>
+
+            <div className="right-operating flex-start-center">
+                <div className="operat-item hover-item">新增计划</div>
+            </div>
+        </div>
     }
 }
