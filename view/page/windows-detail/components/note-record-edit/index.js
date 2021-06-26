db8249fc2f7b55aff39b89844d547a51aed5bb63
rejiejay <454766952@qq.com>
2021-06-26 22:05:34 +0800

    add record-edit-navigation

--- view/page/windows-detail/components/note-record-edit/index.js
+++ view/page/windows-detail/components/note-record-edit/index.js
@@ -6,8 +6,22 @@ export default class NoteRecordEdit extends React.Component {
     render() {
         const { height } = this.props;
 
-        return <div className='note-record-edit noselect' style={{ height: `${height}px` }}>
-            NoteRecordEdit
+        return <div className='note-record-edit flex-start' style={{ minHeight: `${height}px` }}>
+            <div className='record-edit-navigation noselect' style={{ height: `${height}px` }}>
+                <div className='edit-navigation-container'>
+                    <div className='edit-navigation-h1'>标题1</div>
+                    <div className='edit-navigation-h2'>标题2</div>
+                    <div className='edit-navigation-h1'>标题1</div>
+                    <div className='edit-navigation-h2'>标题2</div>
+                    <div className='edit-navigation-h1'>标题1</div>
+                    <div className='edit-navigation-h2'>标题2</div>
+                    <div className='edit-navigation-h3'>标题3</div>
+                    <div className='edit-navigation-h1'>标题1</div>
+                    <div className='edit-navigation-h2'>标题2</div>
+                    <div className='edit-navigation-h3'>标题3</div>
+                    <div className='edit-navigation-h4'>标题4</div>
+                </div>
+            </div>
         </div>
     }
 }
