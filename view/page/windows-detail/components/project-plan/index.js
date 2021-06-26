0db38e32a8e511db85b601286634ed35c3bd9de2
rejiejay <454766952@qq.com>
2021-06-26 19:00:05 +0800

    add ColumnMainItem

--- view/page/windows-detail/components/project-plan/index.js
+++ view/page/windows-detail/components/project-plan/index.js
@@ -1,5 +1,22 @@
 import { AddIcon } from './svg';
 
+const ColumnMainItem = ({ name }) => {
+    return <div className='column-main-item flex-center'>
+        <label>{name}</label>
+        <AddIcon className='add-main-plan' />
+        <AddIcon className='add-sub-plan' />
+    </div>
+}
+
+const PlanColumnItem = ({ mainName, isMain }) => {
+    return <div className='project-plan-column flex-start'>
+        {!isMain && <div className='column-line' />}
+        <div className='column-main flex-column'>
+            <ColumnMainItem name={mainName} />
+        </div>
+    </div>
+}
+
 export default class ProjectPlan extends React.Component {
     constructor(props) {
         super(props)
@@ -9,11 +26,11 @@ export default class ProjectPlan extends React.Component {
     render() {
         return <div className='project-plan noselect'>
             <div className='project-plan-container flex-start'>
-                <div className='project-plan-main flex-center'>
-                    <label>整体目标</label>
-                    <AddIcon className='add-main-plan' />
-                    <AddIcon className='add-sub-plan' />
-                </div>
+                <PlanColumnItem mainName='整体目标' isMain />
+                <PlanColumnItem mainName='第一目标' />
+                <PlanColumnItem mainName='第二目标' />
+                <PlanColumnItem mainName='第三目标' />
+                <PlanColumnItem mainName='第四目标' />
             </div>
         </div>
     }
